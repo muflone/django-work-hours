6fd9a1d2b25b63fcba3f28001c920c1e4415f945
Muflone <webreg@muflone.com>
2022-02-05 20:03:15 +0100

    Add django-debug_toolbar URLs for debug

--- project/urls.py
+++ project/urls.py
@@ -18,10 +18,21 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
 
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    try:
+        # Try to add the Django Debug Toolbar path
+        import debug_toolbar
+        urlpatterns.append(path('__debug__/',
+                                include(debug_toolbar.urls)))
+    except ImportError:
+        # Module not found, don't add the Django Debug Toolbar path
+        pass
