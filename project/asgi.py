7444eddba454dc237c9cd468ae259116757c4635
Muflone <webreg@muflone.com>
2022-02-05 17:48:14 +0100

    Initial Django project

---
+++ project/asgi.py
@@ -0,0 +1,27 @@
+##
+#     Project: Django Work Hours
+# Description: A Django application to manage the employees work hours
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2022 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+application = get_asgi_application()
