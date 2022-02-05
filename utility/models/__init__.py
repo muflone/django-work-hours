9f980569ddd94c1c08fffd30bbf573acfcf1f592
Muflone <webreg@muflone.com>
2022-02-05 18:22:38 +0100

    Added BaseModel model

--- utility/models/__init__.py
+++ utility/models/__init__.py
@@ -17,3 +17,5 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
+
+from .base_model import BaseModel                                  # noqa: F401
