b7335c0ab464db8d97680b41a094fd7ffd46dec6
Muflone <webreg@muflone.com>
2022-02-05 23:24:38 +0100

    Added RequireSuperUser mixin

--- utility/mixins/require_superuser.py
+++ utility/mixins/require_superuser.py
@@ -18,6 +18,15 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
 
-from .generic import GenericMixin                                  # noqa: F401
-from .require_login import RequireLoginMixin                       # noqa: F401
-from .require_superuser import RequireSuperUserMixin               # noqa: F401
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.urls import reverse_lazy
+
+
+class RequireSuperUserMixin(UserPassesTestMixin):
+    """
+    Require a login with superuser access
+    """
+    login_url = reverse_lazy('workhours.auth.login')
+
+    def test_func(self):
+        return self.request.user.is_superuser
