2b7f4fe062accd34f7e95c7451785b910cddb42e
Muflone <webreg@muflone.com>
2022-02-26 23:33:30 +0100

    Added functions iter_dates and iter_days to iterate over two dates

--- utility/extras/__init__.py
+++ utility/extras/__init__.py
@@ -21,4 +21,4 @@
 from .get_configuration_value import (get_configuration_bool,      # noqa: F401
                                       get_configuration_int,       # noqa: F401
                                       get_configuration_value)     # noqa: F401
-from .iter_dates import iter_dates                                 # noqa: F401
+from .iter_dates import iter_dates, iter_days                      # noqa: F401
--- utility/extras/iter_dates.py
+++ utility/extras/iter_dates.py
@@ -19,16 +19,30 @@
 ##
 
 import datetime
-from typing import Iterable
+from typing import Generator
 
 
 def iter_dates(starting_date: datetime.date,
-               ending_date: datetime.date) -> Iterable[datetime.date]:
+               ending_date: datetime.date) -> Generator[datetime.date,
+                                                        None,
+                                                        None]:
     """
     Iterate over two dates
     :param starting_date: initial date
     :param ending_date: ending date
     :return: each date between the two dates
     """
-    for day in range(int((ending_date - starting_date).days)):
+    for day in iter_days(starting_date=starting_date, ending_date=ending_date):
         yield starting_date + datetime.timedelta(days=day)
+
+
+def iter_days(starting_date: datetime.date,
+              ending_date: datetime.date) -> Generator[int, None, None]:
+    """
+    Iterate the days over two dates
+    :param starting_date: initial date
+    :param ending_date: ending date
+    :return: relative day numbers between the two dates
+    """
+    for day in range(int((ending_date - starting_date).days) + 1):
+        yield day
